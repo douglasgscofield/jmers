5bbc2bda7ee9822557b527525cc198980b54b10c
Douglas Scofield <douglasgscofield@gmail.com>
2016-09-15 10:51:08 +0200

    jsim: add errors to simulated reads

--- testdata/jsim/README.md
+++ testdata/jsim/README.md
@@ -6,41 +6,42 @@ sequence with known error rates.  Join the reads into a single read with a
 known, randomised join position.  Then, create a Jellyfish kmer database for
 the reference and attempt to identify the join position within the read.
 
-I've made Heng Li's `wgsim` read simulator be a submodule here.  To get started
-here, compile as recommended.
+I've made Heng Li's `wgsim` read simulator and Nils Homer's `dwgsim` extension
+of it both submodules here.  `dwgsim` is used by default.  To build both:
 
-    cd wgsim
-    gcc -g -O2 -Wall -o wgsim wgsim.c -lz -lm
-    cd ..
+    make .init
 
-The `jsim.pl` script does the rest.
+A Jellyfish database is not used here, but an appropriate one can be built for
+the reference sequence in use (its filename must end with `.fasta` or `.fa`) with
+
+    make ref.jf
 
 For a reference, I've chosen the fosmid scaffold 999.fasta from the Cossu et
-al. dataset S4, containing a single 41853 bp sequence with no N.
+al. dataset S4, containing a single 41853 bp sequence with no N.  The `999.jf`
+Jellyfish database is for this sequence.  See below for more.
 
 
 Error-free reads
 ----------------
 
 As `jsim.pl` is configured now, it simulates the fragment assuming error-free
-non-overlapping read pairs.  Read pairs are simulated into the files `r1.fq`
-and `r2.fq` using the options `wgsim -N5 -d1000 -s0 -1250 -2250 -e0 -R0
-999.fasta r1.fq r2.fq` and then butt-joined into a single fragment.  Then, a
-normal deviate *t* of mean 100 and standard deviation 20 is drawn and rounded
-and this many bases are trimmed from the left end of the fragment, with 100 -
-*t* bases trimmed from the right end.  The resulting fragment is 400 bp in
-length.
+non-overlapping read pairs.  Read pairs are simulated, and then butt-joined
+into a single fragment.  Then, *t*=100 bases are trimmed from the ends of the
+fragment: a normal deviate *d* of mean *t*/2 and standard deviation 10 is drawn
+and rounded and this many bases are trimmed from the left end of the fragment,
+with *t* - *d* bases trimmed from the right end. The trim amount is truncated
+of it would exceed [0, *t*]. The resulting fragment is 400 bp in length.
 
 The position of the butt-join is tracked through this trim process and the
 fragment is output using a read name having the `/1` of the first readname
 replaced with `_j:###` where `###` is the position within the sequence
 (0-based) where the second read starts.
 
-To simulate 5 error-free reads, which results from the current defaults:
+To simulate 100 reads, error-free:
 
-    ./jsim.pl 999.fasta > simreads.fq
+    ./jsim.pl -N 100 999.fasta > 999_joined_e0.fq
 
-To simulate more reads, provide the `-N` option. Two example simulated reads:
+Two example simulated reads:
 
 ```
 @999_27610_28609_0:0:0_0:0:0_7_j:197
@@ -53,3 +54,52 @@ AATGTAGCTGAATTCTAGCTTCAATTATTTTCCAACATCCTCGAGAATGATCAATTAATCGAGAAGTTGCACAATGCGTT
 IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 
 ```
+
+
+Reads with base substitution errors
+-----------------------------------
+
+Specify a `-e` value.  We can simulate mutations in the reference sequence with
+`-r`, but that is not (yet) what we want for `jmers` testing.
+
+    ./jsim.pl -e 0.01 999.fasta > 999_joined_e0.01.fq
+
+
+Building a Jellyfish database
+-----------------------------
+
+    make 999.jf
+
+Using the following gmake Makefile.  Should the database be canonical?  Any other options?
+
+    JELLYFISH= ../../Jellyfish/bin/jellyfish
+    .SUFFIXES:.fasta .fa  .jf
+
+    .fasta.jf:
+        $(JELLYFISH) count -m 21 -t 1 $< --out-counter-len 2 -o $@
+
+    .fa.jf:
+        $(JELLYFISH) count -m 21 -t 1 $< --out-counter-len 2 -o $@
+
+
+Fosmid end orientation?
+-----------------------
+
+We can sort out the orientation of sequences on the joined fosmid ends, but
+what is the original orientation of the ends with respect to the genome?  For
+a joined fosmid end fragment with an identified split between `AB`:
+
+    A>A>A>AB>B>B>B
+
+which is the original genome orientation and complement orientation (lowercase
+is reverse-complement)?
+
+    A>A>A>A   B>B>B>B
+    A>A>A>A   b<b<b<b
+    a<a<a<a   b<b<b<b
+    a<a<a<a   B>B>B>B
+
+The circularisation implies an orientation, of course, but I can't sort it out
+just now.  When `dwgsim` is used (the default) 'mate pair' orientation is used,
+which is the first of the four.  With `wgsim` or `-o 0`, the second is used.
+
--- testdata/jsim/jsim.pl
+++ testdata/jsim/jsim.pl
@@ -17,29 +17,81 @@ use Math::Random;
 # randomly trim fixed total of 100bp from both ends of fragment, tracking join location
 #   pick length of left end from N(mean=50,sd=10) and set right end to 100 - left
 #   keep track of join location
-#
-# when later adding errors, something like:
-# ./wgsim -N10 -s0 -e0 -r0 -d400 -1250 -2250 -R0 fosmids/999.fasta r1.fq r2.fq
-# wgsim overlapping reads from each segment
+# TODO?: simulate overlapping MiSeq reads and use FLASH
 
-my $ref;
-my ($read1, $read2) = qw/ r1.fq r2.fq /;
-my $o_N = 5;  # number of read pairs to simulate
+my $o_N = 1;  # number of read pairs to simulate
 my $o_pair_outer = 1000;
-my $o_pair_se = 0;
+my $o_pair_sd = 0;
 my $o_readlen = 250;
 my $o_trim = 100;
-my $o_trim_sd = 20;
+my $o_trim_sd = 10;
+my $o_base_error_rate = 0;
+my $o_mutation_rate = 0;
 my $o_seed = 1;
-GetOptions(
-    "N=i" => \$o_N,
-) or die "unknown option: $!";
-$ref = shift if @ARGV;
-die "cannot locate reference fasta $ref" if ! -f $ref;
+my $o_orientation = 1;
+my $o_wgsim = 0;
 
+my $N_trunc = 0;
+my $usage = "
+USAGE:  $0  [ options ] ref.fasta > simreads_joined.fq
+
+Simulated joined read fragments are written to stdout in FastQ format.  Under
+most circumstances we want to specify sequencing error rate with -e, not
+reference mutation rate with -r.
+
+  -N INT     number of read pairs/fragments to simulate [default $o_N]
+  -e FLOAT   per-base sequencing error rate [default $o_base_error_rate]
+  -r FLOAT   per-base mutation to apply to reference [default $o_mutation_rate]
+  -d INT     read pair outer insert size [default $o_pair_outer]
+  -s INT     read pair insert size standard deviation [default $o_pair_sd]
+  -l INT     read length (applies to both reads of a pair) [default $o_readlen]
+  -t INT     total bases to trim from both ends of read fragment [default $o_trim]
+  -T INT     half-trim standard deviation [default $o_trim_sd]
+  -o 0 or 1  read orientation 0: F,R, 1: F,F [dwgsim only, default $o_orientation]
+  -w         use wgsim (https://github.com/lh3/wgsim) to simulate reads; default
+             is dwgsim (https://github.com/nh13/DWGSIM).  Both are submodules.
+
+The end trim (controlled with -t and -T) trims t bases in total from the ends
+of the fragment.  The left trim is t_L=Normal(mean=t/2,sd=T) and the right trim
+is t_R=t-t_L.  The net effect is to produce consistently sized fragments for
+which the join location is randomised.  If t_L<0 or t_L>t, it is truncated; a
+message is produced if more than 1% of trims are truncated.
+
+No indels are simulated (-R0 to dwgsim and wgsim).
+
+";
+
+GetOptions(
+    "d|pair-outer=i"        => \$o_pair_outer,
+    "e|base-error-rate=f"   => \$o_base_error_rate,
+    "l|read-len=i"          => \$o_readlen,
+    "N=i"                   => \$o_N,
+    "o|orientation=i"       => \$o_orientation,
+    "r|mutation-rate=f"     => \$o_mutation_rate,
+    "s|pair-sd=i"           => \$o_pair_sd,
+    "t|trim=i"              => \$o_trim,
+    "T|trim-sd=i"           => \$o_trim_sd,
+    "w|wgsim"               => \$o_wgsim,
+) or die "unknown option: $!";
+die "$usage" if ! @ARGV;
+my $ref = shift @ARGV;
+die "cannot locate reference fasta $ref" if ! -f $ref;
+my ($prefix, $read1, $read2);
+if ($o_wgsim) {
+    ($read1, $read2) = ("read_wgsim.read1.fastq",      "read_wgsim.read2.fastq");
+} else {
+    $prefix = "read_dwgsim";
+    ($read1, $read2) = ("$prefix.bwa.read1.fastq", "$prefix.bwa.read2.fastq");
+}
+#
 # Simulate non-overlapping read ends
-my $cmd = "./wgsim/wgsim -S$o_seed -N$o_N -d$o_pair_outer -s$o_pair_se -1$o_readlen -2$o_readlen -e0 -R0 $ref $read1 $read2 > /dev/null";
-say STDERR "wgsim command: $cmd";
+my $cmd;
+if ($o_wgsim) {
+    $cmd = "./wgsim/wgsim -S$o_seed -N$o_N -d$o_pair_outer -s$o_pair_sd -1$o_readlen -2$o_readlen -e$o_base_error_rate -R0 -r$o_mutation_rate $ref $read1 $read2 > /dev/null";
+} else {
+    $cmd = "./DWGSIM/dwgsim -z$o_seed -N$o_N -d$o_pair_outer -s$o_pair_sd -1$o_readlen -2$o_readlen -e$o_base_error_rate -E$o_base_error_rate -R0 -r$o_mutation_rate -y0 -S$o_orientation $ref $prefix > /dev/null";
+}
+say STDERR "sim command: $cmd";
 system($cmd);
 
 # Merge together
@@ -60,8 +112,9 @@ while (<$r1>) {
     my $q = $r1_4 . $r2_4;
     my $joinpos = $o_readlen; # note this is 0-based
     my $lefttrim = sprintf "%.0f", Math::Random::random_normal(1, $o_trim/2, $o_trim_sd);
+    if    ($lefttrim < 0)       { $lefttrim = 0;       ++$N_trunc; }
+    elsif ($lefttrim > $o_trim) { $lefttrim = $o_trim; ++$N_trunc; }
     my $righttrim = $o_trim - $lefttrim;
-    die "some trim length negative" if $lefttrim < 0 or $righttrim < 0;
     $s = substr $s, $lefttrim; $s = substr $s, 0, -$righttrim;
     $q = substr $q, $lefttrim; $q = substr $q, 0, -$righttrim;
     $joinpos -= $lefttrim;
@@ -74,3 +127,6 @@ while (<$r1>) {
 
     print STDOUT "$m_1\n$m_2\n$m_3\n$m_4\n";
 }
+
+say STDERR "*** $N_trunc of $o_N trim lengths were truncated to keep trim to $o_trim bases" if ($N_trunc / $o_N) > 0.01;
+
